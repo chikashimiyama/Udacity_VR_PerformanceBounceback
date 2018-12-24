cc9ea798cf5d224fc324a667b56d7b34f70e1e67
Chikashi Miyama <chikashimiyama@gmail.com>
2018-12-24 17:34:16 +0900

    remove instantiate entirely from the BallSpawn logic

--- Assets/PerformanceBounceback/Scripts/BallSpawner.cs
+++ Assets/PerformanceBounceback/Scripts/BallSpawner.cs
@@ -1,69 +1,55 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BallSpawner : MonoBehaviour
 {
-    public static BallSpawner current;
-
     public GameObject pooledBall; //the prefab of the object in the object pool
-    public int ballsAmount = 20; //the number of objects you want in the object pool
-    public List<GameObject> pooledBalls; //the object pool
-    private static int ballPoolNum_; //a number used to cycle through the pooled objects
+    
+    private const int BallsAmount = 1000; 
+    private GameObject[] pooledBalls_;
+    private static int _poolIndex = -1; 
 
+    
     private float cooldown_;
     private const float CooldownLength = 0.5f;
 
-    private void Awake()
-    {
-        current = this; //makes it so the functions in ObjectPool can be accessed easily anywhere
-    }
+
 
     private void Start()
     {
-        pooledBalls = new List<GameObject>();
-        for (var i = 0; i < ballsAmount; i++)
+        pooledBalls_ = new GameObject[BallsAmount];
+        for (var i = 0; i < pooledBalls_.Length; ++i)
         {
-            var obj = Instantiate(pooledBall);
-            obj.SetActive(false);
-            pooledBalls.Add(obj);
+            pooledBall = Instantiate(pooledBall);
+            pooledBall.SetActive(false);
+            pooledBall.transform.parent = transform;
+            pooledBall.name = "ball" + i.ToString();
+            pooledBalls_[i] = pooledBall;
         }
+        System.GC.Collect(); // to clean garbage possibly created by string
     }
 
     private GameObject GetPooledBall()
     {
-        ballPoolNum_++;
-        if (ballPoolNum_ > (ballsAmount - 1))
+        _poolIndex++;
+        if (_poolIndex > (BallsAmount - 1))
         {
-            ballPoolNum_ = 0;
+            _poolIndex = 0;
         }
-
-        //if we’ve run out of objects in the pool too quickly, create a new one
-        if (pooledBalls[ballPoolNum_].activeInHierarchy)
-        {
-            //create a new bullet and add it to the bulletList
-            var obj = Instantiate(pooledBall);
-            pooledBalls.Add(obj);
-            ballsAmount++;
-            ballPoolNum_ = ballsAmount - 1;
-        }
-
-        return pooledBalls[ballPoolNum_];
+        return pooledBalls_[_poolIndex];
     }
 
     private void Update()
     {
         cooldown_ -= Time.deltaTime;
-        if (cooldown_ <= 0)
-        {
-            cooldown_ = CooldownLength;
-            SpawnBall();
-        }
+        if (!(cooldown_ <= 0)) return;
+        cooldown_ = CooldownLength;
+        SpawnBall();
     }
 
     private void SpawnBall()
     {
-        var selectedBall = current.GetPooledBall();
-        selectedBall.transform.position = transform.position;
+        var selectedBall = GetPooledBall();
+        selectedBall.transform.localPosition = Vector3.zero;
         var selectedRigidbody = selectedBall.GetComponent<Rigidbody>();
         selectedRigidbody.velocity = Vector3.zero;
         selectedRigidbody.angularVelocity = Vector3.zero;
